f42f420055682812c3375a5853dc2d8bccee9c6d
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-10-09 19:18:42 +0300

    callback

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
-const arr = [1, 2, 3, 4, 5]
+const data = [
+    {
+        name: "kovacs janos",
+        age: 29,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 5
+            },
+            {
+                subjectName: "Physics",
+                subjectGrade: 3
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 2
+            }
+        ]
+    },
+    {
+        name: "kis peter",
+        age: 16,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 2
+            },
+            {
+                subjectName: "Physics",
+                subjectGrade: 5
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 4
+            }
+        ]
+    },    {
+        name: "lefold elek",
+        age: 65,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 4
+            },
+            {
+                subjectName: "Physics",
+                subjectGrade: 2
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 5
+            }
+        ]
+    }
+]
 
-/* function returnDouble(number) {
-    return number * 2
-}
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(returnDouble(arr[i]))
+/* for(let i=0;i<data.length;i++) {
+    console.log(data[i].name)
 } */
 
+//data.forEach(student => console.log(student.name))
 
-//const returnDouble2 = number => number * 2  //ha egy elemrol szol a fuggveny, es egy keplet , nincs zarojel es egy sorba lehet irni
-   
-
-/* function logDouble(number) {
-    console.log(number * 2)
-}
-
-arr.forEach(logDouble) */
-
-// inline callback
-
-//arr.forEach(number => console.log(number * 2))
-
-
+data.forEach(student => {
+    console.log(student.name)
+    student.subjects.forEach(subject => console.log(`${subject.subjectName}: ${subject.subjectGrade}`))
+})
